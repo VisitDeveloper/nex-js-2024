8928bd5a6828af9b1c991357c1f9149a9c13baed
fire_full <amiralihatami220@gmail.com>
2025-02-04 15:32:50 +0200

    added auth page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "provider/ThemeProvider";
 import { dir } from "i18next";
 import { AnimatePresence } from "motion/react";
 import { DynamicObjectLiterals, MainLayout } from "components";
+import { usePathname } from 'next/navigation';
 
 export default function RootLayout({
   children,
@@ -17,6 +18,9 @@ export default function RootLayout({
 }>) {
 
   // lang={lng} dir={dir(lng)}
+  const pathname = usePathname();
+  const isAuthPage = pathname.startsWith('/auth');
+
   return (
     <html lang='en' dir='ltr'>
       <body
@@ -45,8 +49,20 @@ export default function RootLayout({
             <MainLayout className='overflow-x-hidden max-h-screen'>
               <DynamicObjectLiterals type='HeaderLayout' />
               {children}
-              <DynamicObjectLiterals type='FooterLayout' />
+              {!isAuthPage && <DynamicObjectLiterals type='FooterLayout' />}
             </MainLayout>
+            {/* <motion.div key={pathname}
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+              }}
+
+              exit={{ opacity: 0 }}
+              transition={{
+                duration: 0.5
+              }}
+              className="bg-mainBackground"> */}
+            {/* </motion.div> */}
           </AnimatePresence>
         </ThemeProvider>
       </body>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,9 @@
 'use client';
 import Header from 'components/elements/header';
-import { ListSetup, SwitchSimpleTheme, Button, Div } from 'components';
+import { ListSetup, SwitchSimpleTheme, Button } from 'components';
 import React, { JSX } from 'react'
 import { Home, InfoCircle, MessageEdit, MobileProgramming, NoteText } from 'iconsax-react';
 import Link from 'next/link';
-import { useTheme } from 'next-themes';
-// import { useParams } from 'next/navigation';
 
 export interface RouteHeader {
   id: number;
@@ -17,7 +15,7 @@ export interface RouteHeader {
 
 
 export default function HeaderLayout({ ...props }) {
-  // const params = useParams()
+
 
   const ArrayRouteHeader: Array<RouteHeader> = [
     {
@@ -55,16 +53,14 @@ export default function HeaderLayout({ ...props }) {
     },
 
   ]
-  const { theme } = useTheme()
   return (
     <>
       {/* mb-[100px] */}
       {/* Desktop */}
-      <Div className=" z-50 rounded-3xl mx-10 hidden md:block">
+      <div className=" z-50 rounded-3xl mx-10 hidden md:block">
         <Header {...props}
-          style={{ backgroundColor: theme === 'dark' ? "rgba(15, 15, 15, 0.6)" : 'rgba(255, 255, 255, 0.32)', }}
-
-          className="backdrop-blur-sm fixed top-0 right-0 lg:right-[17px] left-0 rounded-3xl mt-2 h-[70px] flex
+          className="backdrop-blur-sm 
+                    dark:bg-blackRgba fixed top-0 right-0 lg:right-[17px] left-0 rounded-3xl mt-2 h-[70px] flex
           justify-between items-center px-5 lg:px-10 z-50  mx-5 lg:mx-10 border-2 border-solid border-iconColor">
           <ListSetup alignItems="center" direction="row" justifyContent="start" className="gap-2 lg:gap-5">
             <span className="lg:text-4xl text-2xl font-thin">
@@ -93,16 +89,15 @@ export default function HeaderLayout({ ...props }) {
             <span>
               <SwitchSimpleTheme />
             </span>
-            <Button variant={"secondary"} size={"sm"}> Login</Button>
+            <Link href={'/auth'} className='h-8 px-3 text-xs inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border-solid border-[1px] border-iconColor bg-secondary text-secondary-foreground shadow-sm hover:bg-itemFormBackground '>Login</Link>
           </ListSetup>
 
         </Header>
-      </Div>
+      </div>
 
 
       {/* Mobile */}
-      <Div className='md:hidden block z-50 backdrop-blur-sm fixed top-0 right-0 left-0 h-[60px] px-5'
-        style={{ backgroundColor: theme === 'dark' ? "rgba(15, 15, 15, 0.6)" : 'rgba(255, 255, 255, 0.32)', }}
+      <div className='md:hidden dark:bg-blackRgba block z-50 backdrop-blur-sm fixed top-0 right-0 left-0 h-[60px] px-5'
       >
         <ListSetup alignItems='center' direction='row' justifyContent='between' className='gap-4 h-[60px]'>
           <span className="lg:text-4xl text-2xl font-thin">
@@ -112,11 +107,10 @@ export default function HeaderLayout({ ...props }) {
             <span>
               <SwitchSimpleTheme />
             </span>
-            <Button variant={"secondary"} size={"sm"}> Login</Button>
-
+            <Link href={'/auth'} className='h-8 px-3 text-xs inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border-solid border-[1px] border-iconColor bg-secondary text-secondary-foreground shadow-sm hover:bg-itemFormBackground '>Login</Link>
           </div>
         </ListSetup>
-      </Div>
+      </div>
     </>
   )
 }
