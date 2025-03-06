38bd7e53645d0f96f6e332635f38ad7e4584d401
Shahabi Pour <muh.shahabipour@gmail.com>
2025-03-06 03:01:07 +0200

    fix hiro

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,187 +1,230 @@
-import Footer from 'components/elements/footer'
-import ListSetup from 'components/wrapper-elements/ListSetup';
-import Row from 'components/wrapper-elements/Row';
-import { Span } from 'components/index';
-import React from 'react'
-import Link from 'next/link';
-import { Call, Clock, Facebook, Home, InfoCircle, Instagram, Location, MessageEdit, MobileProgramming, NoteText, Send2, Sms, Whatsapp } from 'iconsax-react';
-import { RouteHeader } from './Header';
+import {
+  Call,
+  Clock,
+  Facebook,
+  Home,
+  InfoCircle,
+  Instagram,
+  Location,
+  MessageEdit,
+  MobileProgramming,
+  NoteText,
+  Send2,
+  Sms,
+  Whatsapp,
+} from "iconsax-react";
 
+import Footer from "components/elements/footer";
+import Image from "next/image";
+import Link from "next/link";
+import ListSetup from "components/wrapper-elements/ListSetup";
+import React from "react";
+import { RouteHeader } from "./Header";
+import Row from "components/wrapper-elements/Row";
+import { Span } from "components/index";
+import { cn } from "lib/utils";
 
 export default function FooterLayout({ ...props }) {
-
   const ArrayRouteFooter: Array<RouteHeader> = [
     {
       id: 0,
-      name: 'Home',
+      name: "Home",
       route: `/`,
-      icon: <Home size="20" className="text-iconColor" />
+      icon: <Home size="20" className="text-iconColor" />,
     },
     {
       id: 1,
-      name: 'Weblog',
+      name: "Weblog",
       route: `/`,
-      icon: <MessageEdit size="20" className='text-iconColor' />
+      icon: <MessageEdit size="20" className="text-iconColor" />,
     },
     {
       id: 2,
-      name: 'Applications',
+      name: "Applications",
       route: `/application-page`,
-      icon: <MobileProgramming size="20" className='text-iconColor' />
-
+      icon: <MobileProgramming size="20" className="text-iconColor" />,
     },
     {
       id: 3,
-      name: 'About',
+      name: "About",
       route: `/about`,
-      icon: <InfoCircle size="20" className='text-iconColor' />
-
+      icon: <InfoCircle size="20" className="text-iconColor" />,
     },
     {
       id: 4,
-      name: 'Contact',
-      route: '/contact-us',
-      icon: <NoteText size="20" className='text-iconColor' />
-
+      name: "Contact",
+      route: "/contact-us",
+      icon: <NoteText size="20" className="text-iconColor" />,
     },
-
-  ]
+  ];
 
   return (
     <>
-      <Footer {...props} className='mb-24 lg:mb-8'>
-        <div className=' dark:text-white  bg-miniBackground  text-black h-auto py-8 mx-10 shadow-lg rounded-3xl'>
-          <Row className='px-10 gap-4'>
-            <div className='md:col-span-4 col-span-12'>
-              <ListSetup className='gap-4' alignItems='start' direction='col' justifyContent='start'  >
-                <div className='font-thin text-3xl mt-5'>
-                  Address
-                </div>
-                <Span className='w-full h-[2px] dark:bg-slate-700  bg-slate-200 mt-[-18px]'></Span>
+      <Footer
+        {...props}
+        className={cn(
+          "text-black bg-gradient-to-br from-[#FEF6EC] to-[#EDFEFC] relative pb-32",
+          "before:absolute before:z-[1] before:content-[''] before:bg-[url('/pattern-wave.png')] before:bg-repeat-x before:w-full before:bottom-8 before:left-0 before:h-6"
+        )}
+      >
+        <div className="h-auto py-8 pt-32 max-w-screen-2xl mx-auto">
+          <Row className="gap-4">
+            <div className="md:col-span-4 col-span-12">
+              <ListSetup
+                className="gap-4"
+                alignItems="start"
+                direction="col"
+                justifyContent="start"
+              >
+                <div className="font-bold text-3xl mt-5">Address</div>
 
-                <ListSetup direction='col' justifyContent='start' alignItems='start'>
-                  <ListSetup direction='row' justifyContent='start' alignItems='center' className='gap-2'>
-                    <Location size="20" className='text-iconColor' />
-                    <div className='text-lg'>
-                      Location
-                    </div>
+                <ListSetup
+                  direction="col"
+                  justifyContent="start"
+                  alignItems="start"
+                >
+                  <ListSetup
+                    direction="row"
+                    justifyContent="start"
+                    alignItems="center"
+                    className="gap-2"
+                  >
+                    <Location size="20" className="text-iconColor" />
+                    <div className="text-lg">Location</div>
                   </ListSetup>
-                  <span className='text-sm text-[#8d8d8e]'>
+                  <span className="text-sm text-[#8d8d8e]">
                     description dolores autdolore veritatis porro
                   </span>
                 </ListSetup>
-                <ListSetup direction='col' justifyContent='start' alignItems='start'>
-                  <ListSetup direction='row' justifyContent='start' alignItems='center' className='gap-2'>
-                    <Sms size="20" className='text-iconColor' />
-                    <div className='text-lg '>
-                      E-mail
-                    </div>
+                <ListSetup
+                  direction="col"
+                  justifyContent="start"
+                  alignItems="start"
+                >
+                  <ListSetup
+                    direction="row"
+                    justifyContent="start"
+                    alignItems="center"
+                    className="gap-2"
+                  >
+                    <Sms size="20" className="text-iconColor" />
+                    <div className="text-lg ">E-mail</div>
                   </ListSetup>
-                  <span className='text-sm text-[#8d8d8e]'>
+                  <span className="text-sm text-[#8d8d8e]">
                     textemail@yahoo.com
                   </span>
                 </ListSetup>
-                <ListSetup direction='col' justifyContent='start' alignItems='start'>
-                  <ListSetup direction='row' justifyContent='start' alignItems='center' className='gap-2'>
-                    <Call size="20" className='text-iconColor' />
-                    <div className='text-lg '>
-                      Telephone
-                    </div>
+                <ListSetup
+                  direction="col"
+                  justifyContent="start"
+                  alignItems="start"
+                >
+                  <ListSetup
+                    direction="row"
+                    justifyContent="start"
+                    alignItems="center"
+                    className="gap-2"
+                  >
+                    <Call size="20" className="text-iconColor" />
+                    <div className="text-lg ">Telephone</div>
                   </ListSetup>
-                  <span className='text-sm text-[#8d8d8e]'>
-                    +1-50-044-5450
-                  </span>
+                  <span className="text-sm text-[#8d8d8e]">+1-50-044-5450</span>
                 </ListSetup>
 
-                <ListSetup direction='col' justifyContent='start' alignItems='start'>
-                  <ListSetup direction='row' justifyContent='start' alignItems='center' className='gap-2'>
-                    <Clock size="20" className='text-iconColor' />
-                    <div className='text-lg '>
-                      Working-Hours
-                    </div>
+                <ListSetup
+                  direction="col"
+                  justifyContent="start"
+                  alignItems="start"
+                >
+                  <ListSetup
+                    direction="row"
+                    justifyContent="start"
+                    alignItems="center"
+                    className="gap-2"
+                  >
+                    <Clock size="20" className="text-iconColor" />
+                    <div className="text-lg ">Working-Hours</div>
                   </ListSetup>
-                  <span className='text-sm text-[#8d8d8e]'>
+                  <span className="text-sm text-[#8d8d8e]">
                     Mon - Fri 9:00 PM - 17:00 AM
                   </span>
                 </ListSetup>
-
               </ListSetup>
             </div>
 
-            <div className='md:col-span-4 col-span-12'>
-              <ListSetup className='gap-4' alignItems='start' direction='col' justifyContent='start'  >
-                <div className='font-thin text-3xl mt-5'>
-                  Links
+            <div className="md:col-span-4 col-span-12">
+              <ListSetup
+                className="gap-4"
+                alignItems="start"
+                direction="col"
+                justifyContent="start"
+              >
+                <div className="text-3xl mt-5 font-bold">Quick Links</div>
+
+                {ArrayRouteFooter.length !== 0 &&
+                  ArrayRouteFooter.map((item: RouteHeader) => {
+                    return (
+                      <>
+                        <Link
+                          href={`${item.route}`}
+                          key={item.id}
+                          className="flex flex-row items-center gap-2"
+                        >
+                          <div className="flex flex-col">
+                            <span className="relative group text-[#666666]">
+                              {item.name}
+                              <span className="absolute left-0 bottom-0 h-[1px] w-0 bg-iconColor transition-all duration-500 group-hover:w-full" />
+                            </span>
+                          </div>
+                        </Link>
+                      </>
+                    );
+                  })}
+              </ListSetup>
+            </div>
+
+            <div className="md:col-span-4 col-span-12 flex flex-col w-full items-end gap-8">
+              <ListSetup
+                className="gap-4"
+                alignItems="center"
+                direction="col"
+                justifyContent="center"
+              >
+                <div className="font-thin text-3xl mt-5">
+                  <Image alt="logo" src="/logo.png" width={128} height={128} />
                 </div>
-                <Span className='w-full h-[2px] dark:bg-slate-700  bg-slate-200  mt-[-18px]'></Span>
-
-                {ArrayRouteFooter.length !== 0 && ArrayRouteFooter.map((item: RouteHeader) => {
-                  return (
-                    <>
-                      <Link href={`${item.route}`} key={item.id} className="flex flex-row items-center gap-2">
-                        {item.icon}
-                        <div className="flex flex-col">
-                          <span className="relative group">
-                            {item.name}
-                            <span className="absolute left-0 bottom-0 h-[1px] w-0 bg-iconColor transition-all duration-500 group-hover:w-full" />
-                          </span>
-                        </div>
-                      </Link>
-                    </>
-                  )
-                })}
-
-
+                <span className="font-semibold">Brain wave Children</span>
               </ListSetup>
 
-            </div>
-
-            <div className='md:col-span-4 col-span-12'>
-              <ListSetup className='gap-4' alignItems='center' direction='col' justifyContent='center'  >
-                <div className='font-thin text-3xl mt-5'>
-                  Logo
+              <ListSetup
+                alignItems="center"
+                justifyContent="start"
+                direction="row"
+                className="gap-2"
+              >
+                <div className="cursor-pointer">
+                  <Instagram size="20" className="text-iconColor" />
                 </div>
-                {/* <Span className='w-full h-[2px] dark:bg-slate-700  bg-slate-200  mt-[-18px]'></Span> */}
-
-                Name Project
+                <div className="cursor-pointer">
+                  <Whatsapp size="20" className="text-iconColor" />
+                </div>
+                <div className="cursor-pointer">
+                  <Send2 size="20" className="text-iconColor" />
+                </div>
+                <div className="cursor-pointer">
+                  <Facebook size="20" className="text-iconColor" />
+                </div>
               </ListSetup>
-
             </div>
-
           </Row>
 
-          <div className='flex flex-col md:flex-row-reverse justify-between items-center '>
-
-
-            <div className='px-10 gap-2 mt-8 flex flex-row-reverse '>
-              <Span>
-                - Social Media
-              </Span>
-              <ListSetup alignItems='center' justifyContent='start' direction='row' className='gap-2'>
-                <div className='cursor-pointer'>
-                  <Instagram size="20" className='text-iconColor' />
-                </div>
-                <div className='cursor-pointer'>
-                  <Whatsapp size="20" className='text-iconColor' />
-                </div>
-                <div className='cursor-pointer'>
-                  <Send2 size="20" className='text-iconColor' />
-                </div>
-                <div className='cursor-pointer'>
-                  <Facebook size="20" className='text-iconColor' />
-                </div>
-              </ListSetup>
-            </div>
-            <div className='px-10 gap-2 mt-8 text-xs font-bold'>
+          <div className="flex flex-col md:flex-row justify-between items-center ">
+            <div className="gap-2 mt-8 text-xs font-bold">
               © 2025 Brainwave. All rights reserved.
             </div>
           </div>
-
-
         </div>
-
       </Footer>
-
     </>
-  )
+  );
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,18 @@
-'use client';
+"use client";
 
-import { Button, ListSetup, SwitchSimpleTheme } from 'components';
-import { Home, InfoCircle, MessageEdit, MobileProgramming, NoteText } from 'iconsax-react';
-import React, { JSX } from 'react'
+import { Button, ListSetup, SwitchSimpleTheme } from "components";
+import {
+  Home,
+  InfoCircle,
+  MessageEdit,
+  MobileProgramming,
+  NoteText,
+} from "iconsax-react";
+import React, { JSX } from "react";
 
-import Header from 'components/elements/header';
-import Link from 'next/link';
+import Header from "components/elements/header";
+import Image from "next/image";
+import Link from "next/link";
 
 export interface RouteHeader {
   id: number;
@@ -14,98 +21,122 @@ export interface RouteHeader {
   icon: React.ReactElement | JSX.Element | React.ReactNode;
 }
 
-
-
 export default function HeaderLayout({ ...props }) {
-
-
   const ArrayRouteHeader: Array<RouteHeader> = [
     {
       id: 0,
-      name: 'Home',
+      name: "Home",
       route: `/`,
-      icon: <Home size="20" className="text-iconColor" />
+      icon: <Home size="20" className="text-iconColor" />,
     },
     {
       id: 1,
-      name: 'Weblog',
+      name: "Weblog",
       route: `/weblog`,
-      icon: <MessageEdit size="20" className='text-iconColor' />
+      icon: <MessageEdit size="20" className="text-iconColor" />,
     },
     {
       id: 2,
-      name: 'Applications',
+      name: "Applications",
       route: `/application-page`,
-      icon: <MobileProgramming size="20" className='text-iconColor' />
-
+      icon: <MobileProgramming size="20" className="text-iconColor" />,
     },
     {
       id: 3,
-      name: 'About',
+      name: "About",
       route: `/about`,
-      icon: <InfoCircle size="20" className='text-iconColor' />
-
+      icon: <InfoCircle size="20" className="text-iconColor" />,
     },
     {
       id: 4,
-      name: 'Contact',
+      name: "Contact",
       route: `/contact-us`,
-      icon: <NoteText size="20" className='text-iconColor' />
-
+      icon: <NoteText size="20" className="text-iconColor" />,
     },
-
-  ]
+  ];
   return (
     <>
       {/* mb-[100px] */}
       {/* Desktop */}
-      <div className=" z-50 rounded-3xl mx-10 hidden md:block">
-        <Header {...props}
-          className="backdrop-blur-sm 
-                    dark:bg-blackRgba fixed top-0 right-0 lg:right-[17px] left-0 rounded-3xl mt-2 h-[70px] flex
-          justify-between items-center px-5 lg:px-10 z-50  mx-5 lg:mx-10 border-2 border-solid border-iconColor">
-          <ListSetup alignItems="center" direction="row" justifyContent="start" className="gap-2 lg:gap-5">
-            <span className="lg:text-4xl text-2xl font-thin">
-              Logo |
-            </span>
+      <div className="top-0 sticky z-[11] hidden md:block">
+        <Header
+          {...props}
+          className="bg-white w-full dark:bg-blackRgba h-20 px-5 lg:px-10 z-50 flex items-center border-b border-solid border-iconColor"
+        >
+          <div className="max-w-screen-2xl mx-auto flex justify-between items-center w-full">
+            <ListSetup
+              alignItems="center"
+              direction="row"
+              justifyContent="start"
+              className="gap-2 lg:gap-5"
+            >
+              <span className="lg:text-4xl text-2xl font-thin">
+                <Image
+                  src="/logo.png"
+                  className=""
+                  width={64}
+                  height={64}
+                  alt="logo"
+                />
+              </span>
+            </ListSetup>
 
-            {ArrayRouteHeader.map((item: RouteHeader) => {
-              return (
-                <>
-                  <Link href={`${item.route}`} key={item.id} className="flex flex-row items-center gap-2">
-                    {item.icon}
-                    <div className="flex flex-col">
-                      <span className="relative group  text-sm">
-                        {item.name}
-                        <span className="absolute left-0 bottom-0 h-[1px] w-0 bg-iconColor transition-all duration-500 group-hover:w-full" />
-                      </span>
-                    </div>
-                  </Link>
-                </>
-              )
-            })}
+            <ListSetup
+              alignItems="center"
+              direction="row"
+              justifyContent="start"
+              className="gap-2 lg:gap-10"
+            >
+              {ArrayRouteHeader.map((item: RouteHeader) => {
+                return (
+                  <>
+                    <Link
+                      href={`${item.route}`}
+                      key={item.id}
+                      className="flex flex-row items-center gap-2"
+                    >
+                      <div className="flex flex-col">
+                        <span className="relative group font-medium text-base">
+                          {item.name}
+                          <span className="absolute left-0 bottom-0 h-[1px] w-0 bg-iconColor transition-all duration-500 group-hover:w-full" />
+                        </span>
+                      </div>
+                    </Link>
+                  </>
+                );
+              })}
+            </ListSetup>
 
-          </ListSetup>
-
-          <ListSetup alignItems="center" direction="row" justifyContent="end" className="gap-5">
-            <span>
-              <SwitchSimpleTheme />
-            </span>
-            {/* <Link href={'/auth'} className='h-8 px-3 text-xs inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border-solid border-[1px] border-iconColor bg-secondary text-secondary-foreground shadow-sm hover:bg-itemFormBackground '>Login</Link> */}
-          </ListSetup>
-
+            <ListSetup
+              alignItems="center"
+              direction="row"
+              justifyContent="end"
+              className="gap-5"
+            >
+              <Link
+                href={"/auth"}
+                className="px-8 py-2 text-base inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none bg-[#FEF8EC] text-secondary-foreground dark:bg-opacity-10"
+              >
+                Login
+              </Link>
+              <span>
+                <SwitchSimpleTheme />
+              </span>
+            </ListSetup>
+          </div>
         </Header>
       </div>
 
-
       {/* Mobile */}
-      <div className='md:hidden dark:bg-blackRgba block z-50 backdrop-blur-sm fixed top-0 right-0 left-0 h-[60px] px-5'
-      >
-        <ListSetup alignItems='center' direction='row' justifyContent='between' className='gap-4 h-[60px]'>
-          <span className="lg:text-4xl text-2xl font-thin">
-            Logo
-          </span>
-          <div className='flex flex-row gap-2 items-center justify-center'>
+      <div className="md:hidden dark:bg-blackRgba block z-50 backdrop-blur-sm fixed top-0 right-0 left-0 h-[60px] px-5">
+        <ListSetup
+          alignItems="center"
+          direction="row"
+          justifyContent="between"
+          className="gap-4 h-[60px]"
+        >
+          <span className="lg:text-4xl text-2xl font-thin">Logo</span>
+          <div className="flex flex-row gap-2 items-center justify-center">
             <span>
               <SwitchSimpleTheme />
             </span>
@@ -114,5 +145,5 @@ export default function HeaderLayout({ ...props }) {
         </ListSetup>
       </div>
     </>
-  )
+  );
 }
