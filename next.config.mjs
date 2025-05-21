22d84ef4cf98551df0fb6c26acdb8cf458125377
Shahabi Pour <muh.shahabipour@gmail.com>
2025-05-21 23:51:39 +0330

    add new config

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,10 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "api.bwaveedu.com",
+      },
+      {
+        protocol: "https",
         hostname: "placehold.co",
       },
       {
